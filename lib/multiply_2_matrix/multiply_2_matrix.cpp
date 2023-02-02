bdb319506a7b243ccfd3df1ec97dca8e53c24f1f
Wai <choongtuckwai21@gmail.com>
2023-02-02 17:43:44 +0800

    last commit

--- lib/multiply_2_matrix/multiply_2_matrix.cpp
+++ lib/multiply_2_matrix/multiply_2_matrix.cpp
@@ -29,7 +29,8 @@ void multiply_2_matrix(float* output_matrix, float* matrix_1, float* matrix_2,
   // multiply 2 matrix
   // for (int row = 0; row < num_row_matrix_1; row += BLOCK_SIZE) {
   //   for (int col = 0; col < num_col_matrix_2; col += BLOCK_SIZE) {
-  //     for (int pointer = 0; pointer < num_col_matrix_1; pointer += BLOCK_SIZE) {
+  //     for (int pointer = 0; pointer < num_col_matrix_1; pointer +=
+  //     BLOCK_SIZE) {
   //       for (int block_row = row; block_row < row + BLOCK_SIZE; ++row) {
   //         for (int block_col = col; block_col < col + BLOCK_SIZE; ++col) {
   //           for (int block_pointer = pointer;
@@ -43,12 +44,16 @@ void multiply_2_matrix(float* output_matrix, float* matrix_1, float* matrix_2,
   //     }
   //   }
   // }
+  int pointer_output_row;
+  int pointer_output, pointer_matrix_1;
   for (int row = 0; row < num_row_matrix_1; ++row) {
+    pointer_output_row = row * num_col_matrix_2;
+    pointer_matrix_1 = row * num_col_matrix_1;
     for (int col = 0; col < num_col_matrix_2; ++col) {
-      output_matrix[row * num_col_matrix_2 + col]=0;
+      pointer_output = pointer_output_row + col;
       for (int pointer = 0; pointer < num_col_matrix_1; ++pointer) {
-        output_matrix[row * num_col_matrix_2 + col] +=
-            matrix_1[row * num_col_matrix_1 + pointer] *
+        output_matrix[pointer_output] +=
+            matrix_1[pointer_matrix_1 + pointer] *
             matrix_2[pointer * num_col_matrix_2 + col];
       }
     }
