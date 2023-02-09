28eddfa97fd703bc1e9ab0c22b51f33322bfaea7
Wai <choongtuckwai21@gmail.com>
2023-02-09 11:07:03 +0800

    chore: remove unuse code

--- lib/multiply_2_matrix/multiply_2_matrix.cpp
+++ lib/multiply_2_matrix/multiply_2_matrix.cpp
@@ -2,8 +2,6 @@
 
 #include <iostream>
 
-#define BLOCK_SIZE 100
-
 void multiply_2_matrix(float* output_matrix, float* matrix_1, float* matrix_2,
                        int num_row_matrix_1, int num_col_matrix_1,
                        int num_row_matrix_2, int num_col_matrix_2) {
@@ -26,24 +24,6 @@ void multiply_2_matrix(float* output_matrix, float* matrix_1, float* matrix_2,
     return;
   }
 
-  // multiply 2 matrix
-  // for (int row = 0; row < num_row_matrix_1; row += BLOCK_SIZE) {
-  //   for (int col = 0; col < num_col_matrix_2; col += BLOCK_SIZE) {
-  //     for (int pointer = 0; pointer < num_col_matrix_1; pointer +=
-  //     BLOCK_SIZE) {
-  //       for (int block_row = row; block_row < row + BLOCK_SIZE; ++row) {
-  //         for (int block_col = col; block_col < col + BLOCK_SIZE; ++col) {
-  //           for (int block_pointer = pointer;
-  //                block_pointer < pointer + BLOCK_SIZE; ++pointer) {
-  //             output_matrix[(block_row * num_col_matrix_2) + block_col] +=
-  //                 matrix_1[(block_row * num_col_matrix_1) + block_pointer] *
-  //                 matrix_2[(block_pointer * num_col_matrix_2) + block_col];
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
   int pointer_output_row;
   int pointer_output, pointer_matrix_1, pointer_matrix_2;
   for (int row = 0; row < num_row_matrix_1; ++row) {
