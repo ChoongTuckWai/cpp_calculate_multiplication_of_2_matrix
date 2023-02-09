870b1ff919d2d71c173a2d3f03f123853769e7c1
Wai <choongtuckwai21@gmail.com>
2023-02-09 09:07:00 +0800

    perf: lower cache hit

--- lib/multiply_2_matrix/multiply_2_matrix.cpp
+++ lib/multiply_2_matrix/multiply_2_matrix.cpp
@@ -45,16 +45,16 @@ void multiply_2_matrix(float* output_matrix, float* matrix_1, float* matrix_2,
   //   }
   // }
   int pointer_output_row;
-  int pointer_output, pointer_matrix_1;
+  int pointer_output, pointer_matrix_1, pointer_matrix_2;
   for (int row = 0; row < num_row_matrix_1; ++row) {
     pointer_output_row = row * num_col_matrix_2;
     pointer_matrix_1 = row * num_col_matrix_1;
-    for (int col = 0; col < num_col_matrix_2; ++col) {
-      pointer_output = pointer_output_row + col;
-      for (int pointer = 0; pointer < num_col_matrix_1; ++pointer) {
-        output_matrix[pointer_output] +=
-            matrix_1[pointer_matrix_1 + pointer] *
-            matrix_2[pointer * num_col_matrix_2 + col];
+    for (int pointer = 0; pointer < num_col_matrix_1; ++pointer) {
+      pointer_matrix_2 = pointer * num_col_matrix_2;
+      for (int col = 0; col < num_col_matrix_2; ++col) {
+        pointer_output = pointer_output_row + col;
+        output_matrix[pointer_output] += matrix_1[pointer_matrix_1 + pointer] *
+                                         matrix_2[pointer_matrix_2 + col];
       }
     }
   }
