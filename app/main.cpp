038185788ab29627ef351dfb2319677dc4cbf33a
Wai <choongtuckwai21@gmail.com>
2023-02-09 10:57:21 +0800

    remove time count

--- app/main.cpp
+++ app/main.cpp
@@ -6,7 +6,6 @@
 #include <lib/helper_function_matrix/helper_function_matrix.h>
 #include <lib/multiply_2_matrix/multiply_2_matrix.h>
 
-#include <chrono>  //Measure execution time
 #include <iostream>
 
 #define DEFAULT_MATRIX_SIZE 100
@@ -86,29 +85,10 @@ int main(int argc, char* argv[]) {
   float* output_matrix =
       allocate_memory_to_matrix(num_row_input_matrix_1, num_col_input_matrix_2);
 
-  // get output matrice and measure execution time
-  auto start = std::chrono::high_resolution_clock::now();
+  // get output matrice
   multiply_2_matrix(output_matrix, input_matrix_1, input_matrix_2,
                     num_row_input_matrix_1, num_col_input_matrix_1,
                     num_row_input_matrix_2, num_col_input_matrix_2);
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  auto duration =
-      std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-
-  // print Execution Time and First element of matrix
-  std::cout << "First three element of matrix: " << output_matrix[0] << " "
-            << output_matrix[1] << " " << output_matrix[2] << "\n";
-  std::cout
-      << "Last three element of matrix: "
-      << output_matrix[num_row_input_matrix_1 * num_col_input_matrix_2 - 1]
-      << " "
-      << output_matrix[num_row_input_matrix_1 * num_col_input_matrix_2 - 2]
-      << " "
-      << output_matrix[num_row_input_matrix_1 * num_col_input_matrix_2 - 3]
-      << "\n";
-  std::cout << "Execution Time:\n";
-  std::cout << duration.count() / 1000000.0 << " seconds\n";
 
   // deallocate memory of input_matrix_1, input_matrix_2, output_matrix
   deallocate_memory_of_matrix(input_matrix_1);
