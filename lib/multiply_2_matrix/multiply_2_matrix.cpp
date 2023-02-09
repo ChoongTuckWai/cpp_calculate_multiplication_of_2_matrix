26336813110cbd27d2b5c40afc46f0156a23d463
Wai <choongtuckwai21@gmail.com>
2023-02-09 11:17:37 +0800

    fix: check input size

--- lib/multiply_2_matrix/multiply_2_matrix.cpp
+++ lib/multiply_2_matrix/multiply_2_matrix.cpp
@@ -2,23 +2,28 @@
 
 #include <iostream>
 
-void multiply_2_matrix(float* output_matrix, float* matrix_1, float* matrix_2,
-                       int num_row_matrix_1, int num_col_matrix_1,
-                       int num_row_matrix_2, int num_col_matrix_2) {
-  if (num_row_matrix_1 == 0) {
-    std::cout << "cannot be multiplied!\n\n";
-
-    return;
-  }
-  if (num_row_matrix_2 == 0) {
-    std::cout << "cannot be multiplied!\n\n";
-
-    return;
+bool is_matrix_cannot_be_multipied(int num_row_matrix_1, int num_col_matrix_1,
+                                   int num_row_matrix_2, int num_col_matrix_2) {
+  // check if matrix is 0 size
+  if (num_row_matrix_1 == 0 || num_col_matrix_1 == 0 || num_row_matrix_2 == 0 ||
+      num_col_matrix_2 == 0) {
+    return false;
   }
 
   // check if this 2 metrix can be multiplied or not
   // if not, then return none
   if (num_col_matrix_1 != num_row_matrix_2) {
+    return false;
+  }
+
+  return true;
+}
+
+void multiply_2_matrix(float* output_matrix, float* matrix_1, float* matrix_2,
+                       int num_row_matrix_1, int num_col_matrix_1,
+                       int num_row_matrix_2, int num_col_matrix_2) {
+  if (is_matrix_cannot_be_multipied(num_row_matrix_1, num_col_matrix_1,
+                                    num_row_matrix_2, num_col_matrix_2)) {
     std::cout << "cannot be multiplied!\n\n";
 
     return;
